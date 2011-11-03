912d8e022c0bda4741c62d9f9520bf26cea6c438
root <root@debian.localdomain>
2011-11-03 15:59:29 +0800

    modify

--- submit_sample.pl
+++ submit_sample.pl
@@ -13,8 +13,9 @@ die "\nplease check config parameter\n" unless init_gloabl_variable( $conf_file 
 my $sample_dir=$conf->getAttribute('SampleFolder');
 my $dbh=$db_helper->get_db_handle;
 
-my $analytic_dir="/mnt/tslab/packages";
-my $cloud_dir="/mnt/tslab/cloud";
+my $analytic_dir="/mnt/tslab";
+my $cloud_dir="/mnt/cloud";
+my $bakup_dir="/mnt/bakup";
 
 #submit the app to analyzer
 while(1)
@@ -27,9 +28,36 @@ while(1)
         sleep(5);
     }
 
+    &clean_table_data();
     &run_one_time();
 
     sleep(600);			# check the task every 10 minutes
+}
+
+sub clean_table_data{
+    my $sql = qq{
+        SELECT distinct a.task_id FROM task a,task_detail b 
+        WHERE a.task_id=b.task_id AND a.status='done'
+        AND TIMESTAMPDIFF( DAY,DATE(a.done_time), CURDATE() ) >=20
+        order by task_id
+    };
+    my $sth = $dbh->prepare($sql);
+    $sth->execute;
+
+    my $del_task_sql = qq{
+        delete from task where task_id = ?
+    };
+    my $del_task_detail_sql = qq{
+        delete from task_detail where task_id= ?
+    };
+
+    my $task_sth = $dbh->prepare($del_task_sql);
+    my $task_detail_sth = $dbh->prepare($del_task_detail_sql);
+
+    while( my $hashref = $sth->fetchrow_hashref() ){
+        $task_sth->execute( $hashref->{task_id} ) or die $DBI::errstr;       
+        $task_detail_sth->execute( $hashref->{task_id} ) or die $DBI::errstr;
+    }
 }
  
 
@@ -51,14 +79,31 @@ sub run_one_time
         $status='success'; 
         $tarfile="$sample_dir/$hash->{package_name}";
         if( -e $tarfile ){
-            my $cmd="cp $tarfile $analytic_dir/; cp $tarfile $cloud_dir/";
-            $status='fail' and next unless &replace_old_app($tarfile);#save  local copy
-            $status='fail' and next unless &execute_cmd($cmd);
+        	my $retry_times = 0;
             open(FH,">$tarfile.ready") or die "Can't create $tarfile ready file: $!";
             close(FH);
-            $cmd="cp $tarfile.ready $analytic_dir/;cp $tarfile.ready $cloud_dir/";
-            $status='fail' and next unless &execute_cmd($cmd);
+
+            $status='fail' and next unless &replace_old_app($tarfile);#save  local copy
+            RE_COMMIT:{
+                # check every system command
+                unless( 
+                        execute_cmd( "cp $tarfile $analytic_dir/")
+                            or 
+                        execute_cmd( "cp $tarfile $cloud_dir/" )
+                            or 
+                        execute_cmd( "cp $tarfile $bakup_dir/" )
+                            or 
+                        execute_cmd( "cp $tarfile.ready $analytic_dir/")
+                            or 
+                        execute_cmd( "cp $tarfile.ready $cloud_dir/" )
+                ){
+                    ++$retry_times;
+                    $status = 'fail';
+                    redo RE_COMMIT if $retry_times < 3
+                }
+            }
         }
+        $dbh->do("update app_info, task_detail set delivery_time=now() where task_id=$hash->{task_id} and  app_info.app_url_md5 = task_detail.detail_id");
     }continue{
         if ($status eq 'success'){
             unlink($tarfile);           
@@ -66,7 +111,28 @@ sub run_one_time
             unlink("$analytic_dir/$hash->{package_name}");           
             unlink("$cloud_dir/$hash->{package_name}");           
             unlink("$analytic_dir/".$hash->{package_name}.".ready");           
-            unlink("$cloud_dir/".$hash->{package_name}.".ready");           
+            unlink("$cloud_dir/".$hash->{package_name}.".ready");         
+            $hash->{package_name}=~ m/(.+?)__/;
+            my $market_name = $1;  
+            my $market_info = $db_helper->get_market_info($market_name);
+
+            my $sql2 =<<EOF;
+            select 
+            a.detail_id as app_url_md5
+            from task_detail a,task b 
+            where a.task_id= $hash->{task_id}
+            and b.market_id = $market_info->{id}
+            and b.task_type='new_app'
+EOF
+            my $sth = $dbh->prepare($sql2);
+            $sth->execute;
+            while( my $hashref = $sth->fetchrow_hashref ){
+            	$dbh->do(
+            	    "update app_info set status='fail' 
+            	    where app_url_md5=$hashref->{app_url_md5}"
+                );
+            }
+
         }
         unlink("$tarfile.ready");
         $dbh->do("update package set status='$status',end_time=now() where task_id=$hash->{task_id}");
@@ -81,8 +147,8 @@ sub replace_old_app{
     $resp=~s/\n/  /g;
     my $cmd="cd ".$conf->getAttribute("TempFolder").";rm -rf $resp";
     return 0 unless execute_cmd($cmd);
-    $cmd="cd ".$conf->getAttribute("AppFolder").";rm -rf $resp";
+    $cmd="cd $bakup_dir/markets;rm -rf $resp";
     return 0 unless execute_cmd($cmd);
-    $cmd="tar xzvf $tarfile -C ".$conf->getAttribute("AppFolder");
+    $cmd="tar xzvf $tarfile -C $bakup_dir/markets --no-same-owner";
     return execute_cmd($cmd);
 }
